41e62c01451a0a1e71552b2d5f9e2a4257450b05
Gonzayanni <90162125+Gonzayanni@users.noreply.github.com>
2021-12-07 14:27:39 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,457 @@
+
+// let nombre =prompt("decime tu nombre")
+
+// alert("hola " + nombre)
+
+
+
+// operadores en js asignacion
+
+// let numero =10;
+// numero +=5
+// alert(numero)
+
+// let numero2 =10;
+// numero2 -=5
+// alert(numero2)
+
+// let numero3 =15;
+// numero3 %=5
+// alert(numero3)
+
+// let numero4=10;
+// numero4 *=5
+// alert(numero4)
+
+// let numero5 =10;
+// numero5 /=5
+// alert(numero5)
+// let numero6 =10;
+// numero6 **=5
+// alert(numero6)
+
+
+
+// operadores en js arimetricos
+
+// let numero1=10;
+// let numero2=5;
+// resultado=numero1/numero2;
+// alert(resultado)
+
+
+// CONCATENAR NUMEROS
+
+// let num1=10
+// let num2=5
+// res=""+ num1+num2
+
+// document.write(res)
+
+
+
+
+// backticks
+// let nombre="Gonzalo Yanni"
+// let frase=`mi nombre es ${nombre}`
+// alert(frase)
+
+
+
+// operadores logicos devuelven booleanos
+
+
+
+
+
+
+
+                                             // 6/12
+
+
+
+
+
+
+// let numero=3
+// let numero2=4
+
+
+// document.write(numero>numero2)
+// document.write(numero<numero2)
+// document.write(numero>=numero2)
+// document.write(numero<=numero2)
+
+
+
+// operadores logicos & || devuelven booleanos
+
+
+
+// num1= 12
+// num2=24
+// num3=45
+// num4=4
+// num5=9
+
+
+
+// op= (num1 > num2) || num3!= num4;
+// document.write(op)
+
+
+
+// Conditionals
+
+
+
+// let nombre= "gonzalo"
+// let apellido="yanni"
+
+// if (nombre=="gonzalo" && apellido=="yanni") {
+
+//     document.write("tu nombre es " + nombre +" y tu apellido es " + apellido);
+//  }
+
+//  else{
+//      document.write("tu nombre es erroneo")
+//  }
+
+
+
+
+// PPRIMER PROBLEMA DE COFLA CASO A
+
+
+
+
+//   const dineroCofla=prompt("cuanto dinero tienes Cofla");
+//   const dineroJuan=prompt("cuanto dinero tienes Juan");
+//   const dineroRoberto=prompt("cuanto dinero tienes Roberto");
+
+
+   
+
+// // Precios de Helados
+
+
+
+
+//  if (dineroCofla >= 0.6 && dineroCofla <1){
+//     alert( "Cofla te podes comprar el palito de agua");
+//     alert("y te sobran " + (dineroCofla- 0.6))
+// }
+// else if (dineroCofla>= 1 && dineroCofla < 1.6){
+//    alert("Cofla te podes comprar el palito de crema que sale $1USD")
+//    alert("y te sobran " + (dineroCofla- 1))
+
+// }
+
+// else if (dineroCofla>= 1.6 && dineroCofla < 1.7){
+//    alert("Cofla te podes comprar un bombon helado marca heladix que sale $1.6   USD")
+//    alert("y te sobran " + (dineroCofla- 1.6))
+
+// }
+// else if (dineroCofla>= 1.7 && dineroCofla < 1.8){
+//    alert("Cofla te podes comprar  un bombon marca heladovich que sale $1.7USD ")
+//    alert("y te sobran " + (dineroCofla- 1.7))
+
+// }
+// else if (dineroCofla>= 1.8 && dineroCofla < 1.9){
+//    alert("Cofla te podes comprar  un bombon marca helardo que sale $1.7USD ")
+//    alert("y te sobran " + (dineroCofla- 1.8))
+// }
+// else if (dineroCofla>= 2.9){
+//    alert(" Cofla podes comprate el potecito helado con confites o un pote de 1/4 kg que sale $2.9USD");
+//    alert("y te sobran " + (dineroCofla- 2.9))
+
+// }
+
+
+// else{
+//    alert("Cofla sos re pobre man no te alcanza para nada")
+  
+
+// }
+
+
+
+// // ROBERTO
+
+// if (dineroRoberto >= 0.6 && dineroRoberto <1){
+//     alert("Roberto te podes comprar el palito de agua")
+// }
+// else if (dineroRoberto>= 1 && dineroRoberto < 1.6){
+//    alert("Roberto te podes comprar el palito de crema que sale $1USD")
+
+// }
+
+// else if (dineroRoberto>= 1.6 && dineroRoberto < 1.7){
+//    alert("Roberto te podes comprar un bombon helado marca heladix que sale $1.6   USD")
+
+// }
+// else if (dineroRoberto>= 1.7 && dineroRoberto < 1.8){
+//    alert("Roberto te podes comprar  un bombon marca heladovich que sale $1.7USD ")
+
+// }
+// else if (dineroRoberto>= 1.8 && dineroRoberto < 1.9){
+//    alert("Roberto te podes comprar  un bombon marca helardo que sale $1.7USD ")
+
+// }
+// else if (dineroRoberto>= 2.9){
+//    alert(" Roberto podes comprate el potecito helado con confites o un pote de 1/4 kg que sale $2.9USD");
+
+// }
+// else if (dineroRoberto>= 5.8){
+//    alert(" Roberto podes comprate  2 potecitos de  helado con confites que salen $2.9USD cada uno, o dos potes de 1/4 kg que tambien valen $2.9USD cada uno")
+
+// }
+
+
+// else{
+//    alert("Roberto sos re pobre man no te alcanza para nada")
+
+// }
+
+
+
+// // Roberto
+
+// if (dineroJuan >= 0.6 && dineroJuan <1){
+//      alert("Juan te podes comprar el palito de agua")
+//  }
+// else if (dineroJuan>= 1 && dineroJuan < 1.6){
+//     alert("Juan te podes comprar el palito de crema que sale $1USD")
+
+// }
+
+// else if (dineroJuan>= 1.6 && dineroJuan < 1.7){
+//     alert("Juan te podes comprar un bombon helado marca heladix que sale $1.6   USD")
+
+// }
+// else if (dineroJuan>= 1.7 && dineroJuan < 1.8){
+//     alert("Juan te podes comprar  un bombon marca heladovich que sale $1.7USD ")
+
+// }
+// else if (dineroJuan>= 1.8 && dineroJuan < 1.9){
+//     alert("Juan te podes comprar  un bombon marca helardo que sale $1.7USD ")
+
+// }
+// else if (dineroJuan>= 2.9){
+//     alert(" Juan podes comprate el potecito helado con confites o un pote de 1/4 kg que sale $2.9USD");
+
+// }
+// else if (dineroJuan>= 5.8){
+//     alert(" Juan podes comprate  2 potecitos de  helado con confites que salen $2.9USD cada uno, o dos potes de 1/4 kg que tambien valen $2.9USD cada uno")
+
+// }
+
+
+// else {
+//     alert("Juan sos re pobre man no te alcanza para nada")
+
+// }
+
+
+
+//  ARRAYS
+
+
+// let variable=["1","2","3","4","5"]
+
+
+// document.write(variable[2])
+
+
+                                  // 7/12
+
+
+
+//  ARRAYS Asociativos
+
+
+// let computadora={ 
+//    procesador:"ryzen 5 3500g",
+//    memoriaRam:"16GB",
+//    discoDuro:"1TB"
+// };
+
+
+// // document.write(computadora["procesador"]);
+
+
+// let procesador= computadora["procesador"]
+// let discoDuro= computadora["discoDuro"]
+// let memoriaRam= computadora["memoriaRam"]
+
+
+
+// compu=`mi procesador es el:<b> ${procesador} </b> <br>
+// ,mi disco duro tiene una capacidad de: <b> ${discoDuro}</b> <br>
+// y mi memoria ram es de: <b> ${memoriaRam}</b>
+// `
+
+// document.write(compu)
+
+
+// BLUCLES E ITERACION while, for
+
+
+// while 
+
+
+// let numero=0
+// while(numero<1000){
+//     numero++
+//     document.write(numero)
+    
+//     if (numero==10){
+//         break
+//     }
+
+// }
+
+
+// for
+
+
+// for(let i=0; i < 5 ; i++){
+  
+
+
+//     if(i==2){
+//         continue;
+//     }
+//     document.write(i +" <br>")
+// }
+
+
+// for in-for of
+
+// let animales=["gato","perro","tiranosaurioRex"]
+
+
+// for (animal in animales){
+//     document.write(animal +"<br>")
+// }
+
+// for (animal of animales){
+//     document.write(animal +"<br>")
+// }
+
+
+// sentencia label
+
+
+// array1 = ["maria","josefa","juan"]
+// array2 = ["pepe","roberta",`<b> ${array1} </b>`]
+
+// for(let array in array2){
+//     if(array ==2){
+//         for(let array of array1){
+//             document.write(array +"<br>")
+//         }
+//     }
+// }
+
+
+
+// FUNCIONES
+
+
+// 1er forma de crear funciones
+
+
+
+// function saludar(){
+//     respuesta =prompt("Hola Gonzalo.¿Como estas?")
+
+//     if(respuesta=="bien"){
+//         alert("me alegro")
+//     }else if (respuesta=="mal"){
+//         alert("me des-alegro")
+//     }
+// }
+
+
+// saludar()
+// saludar()
+
+
+
+// otro ejemplo
+
+
+
+// function saludar(){
+//     alert("hola")
+//     return "la funcion se ejecutó corretamente"
+
+
+// }
+
+
+// let saludo =saludar()
+
+
+// document.write(saludo)
+
+
+// ejemplo con parametros
+
+
+// function suma(num1,num2){
+//     res=num1 + num2
+//     document.write(res)
+//     document.write("<br>")
+// }
+
+// suma(10,10)
+// suma(20,20)
+// suma(30,30)
+
+
+// function hecha por mi
+
+
+// function saludar(nombre){
+// let frase =`Hola ${nombre},¿Cómo estás?`
+// document.write(frase +"<hr>")
+
+// }
+
+
+// saludar("Gonzalo")
+// saludar("Roberto")
+// saludar("Pedro")
+
+
+
+// Otra Forma De Hacer Funciones 
+
+
+// 2da forma de crear funciones
+
+
+// const saludar =function(nombre){
+//     let frase = `hola como estas ${nombre} ,todo bien?`
+
+//     document.write(frase)
+    
+// }
+// saludar("hola")
+
+
+
+
+// FUNCIONES FLECHA 3er forma de crear funciones
+
+
+
+// const saludar = (nombre)=>{
+// let frase = `hola como estas ${nombre} ,todo bien?`
+
+// document.write(frase)
+// }
+// saludar("gonza")
+
